463cc88f7ab8dbf8bb4a237b5d13cd2bb736b1a0
ALazyPig <zj565405576@hotmail.com>
2016-05-07 15:46:39 +0800

    chat

--- src/com/chat/ChatClient.java
+++ src/com/chat/ChatClient.java
@@ -8,29 +8,56 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ChatClient extends Frame{
 	
 	private TextField textField = new TextField();
 	private TextArea textArea = new TextArea();
-	
+	private static Socket socket;
+	private OutputStream outputStream;
+	private InputStream inputStream;
+	private DataOutputStream dataOutputStream;
+	private DataInputStream dataInputStream;
 	public void launchFrame(){
 		this.setLocation(400, 300);
 		this.setSize(300, 300);
 		add(textField, BorderLayout.SOUTH);
 		add(textArea, BorderLayout.NORTH);
 		pack();
-		this.setVisible(true);
+		this.setVisible(true);	
+		try {
+			outputStream = socket.getOutputStream();
+			inputStream = socket.getInputStream();
+			dataOutputStream = new DataOutputStream(outputStream);
+			dataInputStream = new DataInputStream(inputStream);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		this.addWindowListener(new WindowAdapter(){
 			@Override
 			public void windowClosing(WindowEvent e){
+				/*try {
+					dataInputStream.close();
+					dataOutputStream.close();//流关闭，socket将关闭
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}*/
 				setVisible(false);
 		        System.exit(-1);
+		        
 		    }	
 		});
 		textField.addActionListener(new textFieldListener());
+		new Thread(new Receive()).start();
+		
 	}
 	
 	private class  textFieldListener implements ActionListener{
@@ -38,18 +65,55 @@ public class ChatClient extends Frame{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String s = textField.getText().trim();
-			textArea.setText(s);
-			textField.setText("");
+			//textArea.setText(s);
+			textField.setText("");			
+			try {				
+				dataOutputStream.writeUTF(s);
+				dataOutputStream.flush();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}			
 		}
 		
 	}
 	
-	public static void main(String[] args){
-		new ChatClient().launchFrame();
+	private class Receive implements Runnable{
+
+		@Override
+		public void run() {
+			String s;
+			try {
+				while(true){
+					s = dataInputStream.readUTF();
+					System.out.println(s);
+					textArea.setText(s);
+				}					
+			} catch (EOFException e) {
+				System.out.println("客户端关闭");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally{
+				try {
+					if(dataInputStream != null)
+						dataInputStream.close();
+					if(dataOutputStream != null)
+						dataOutputStream.close();
+					if(socket != null)
+						socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){		
 		try {
-			Socket socket = new Socket("127.0.0.1",9999);
+			socket = new Socket("127.0.0.1",9999);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		new ChatClient().launchFrame();
 	} 
 }
--- src/com/chat/ChatServer.java
+++ src/com/chat/ChatServer.java
@@ -1,23 +1,82 @@
 package com.chat;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
-
+	private List<Clients> clientList = new ArrayList<Clients>();
 	public static void main(String[] args) {
-		try {
-			ServerSocket serverSocket = new ServerSocket(9999);
-			while(true){
-				Socket socket = serverSocket.accept();
-				System.out.println("a client connected");
-			}
+		new ChatServer().start();	
+	}
+	
+	public  void start() {
+		ServerSocket serverSocket = null;		
+		try {		
+			serverSocket = new ServerSocket(9999);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		try {
+			while(true){			
+				Socket socket = serverSocket.accept();	
+				Clients clients = new Clients(socket);
+				clientList.add(clients);
+				new Thread(clients).start();
+				System.out.println("A client connected");					
+			}
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}			
 		
+	}
 
+	private class Clients implements Runnable{
+		private Socket socket;
+		private DataInputStream dataInputStream;
+		private DataOutputStream dataOutputStream;
+		public Clients(Socket socket){
+			this.socket = socket;
+		}
+		@Override
+		public void run() {
+			try {
+				dataInputStream = new DataInputStream(socket.getInputStream());
+				dataOutputStream = new DataOutputStream(socket.getOutputStream());
+				while(true){
+					String s = dataInputStream.readUTF();//读到末尾EOFException				
+					System.out.println(s);
+					for (Clients clients : clientList) {
+						clients.dataOutputStream.writeUTF(s);//客户端失去连接抛异常
+					}
+				}
+			}catch (EOFException e){
+				if(this != null) clientList.remove(this);
+				System.out.println("已关闭");
+			} catch (IOException e1) {
+				System.out.println("客户端关闭");
+			} finally{
+				try {
+					if(dataInputStream != null)
+						dataInputStream.close();
+					if(dataOutputStream != null)
+						dataOutputStream.close();
+					if(socket != null)
+						socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}				
+			}
+		
+		}
+		
+		
 	}
 
 }
